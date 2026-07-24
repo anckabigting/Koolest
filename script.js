7829acb8aef98fccbeb8c480b5da0e8d6de05673
yani-clstn <altheanicolekabigting@gmail.com>
2026-07-24 21:00:03 +0800

    test: images slideshow

--- script.js
+++ script.js
@@ -108,3 +108,22 @@ function submitReport(event) {
   const formSuccess = document.getElementById("formSuccess");
   if (formSuccess) formSuccess.style.display = "block";
 }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  slideIndex++;
+  if (slideIndex > slides.length) { 
+    slideIndex = 1; 
+  }    
+  
+  slides[slideIndex - 1].style.display = "block";  
+  setTimeout(showSlides, 3000); // Changes image every 3 seconds
+}
