8c62a2664306488b2de688ad88534e9ebabc96e5
yani-clstn <altheanicolekabigting@gmail.com>
2026-07-26 18:28:22 +0800

    update: prisma version

--- src/assets/libs/prisma.js
+++ src/assets/libs/prisma.js
@@ -1,17 +1,11 @@
 import { PrismaClient } from "../../generated/prisma/index.js";
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { Pool } from "@neondatabase/serverless";
 
 const globalForPrisma = globalThis;
-// test
-console.log("DATABASE_URL length:", process.env.DATABASE_URL?.length);
-console.log("DATABASE_URL JSON:", JSON.stringify(process.env.DATABASE_URL)); // //
 
-const pool = new Pool({
+const adapter = new PrismaNeon({
   connectionString: process.env.DATABASE_URL,
 });
-
-const adapter = new PrismaNeon(pool);
 
 export const prisma =
   globalForPrisma.prisma ??
@@ -21,4 +15,3 @@ export const prisma =
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
