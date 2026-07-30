478687e7b28d55213a0389ac89b84de5558a93cb
yani-clstn <altheanicolekabigting@gmail.com>
2026-07-30 22:57:26 +0800

    feat (report): zod validation schema

---
+++ src/assets/libs/validations/issue.js
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+export const issueTypeEnum = z.enum([
+  "BROKEN_LINK_OR_BUTTON",
+  "VISUAL_LAYOUT_GLITCH",
+  "FORM_SUBMISSION_ERROR",
+  "MOBILE_SCREEN_DISPLAY",
+  "OTHER_WEBSITE_BUG",
+]);
+
+export const issueReportSchema = z.object({
+  name: z
+  .string()
+  .trim()
+  .optional()
+  .or(z.literal("")),
+
+  email: z
+  .string()
+  .trim()
+  .email("Please enter a valid email address."),
+
+  issueType: issueTypeEnum,
+
+  details: z
+  .string()
+  .trim()
+  .min(5, "Please provide more details about the issue."),
+});
